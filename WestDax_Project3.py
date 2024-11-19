7bb86d275120023c8e7cdd602f60fb16c7e37182
DaxWest <awest49@uwo.ca>
2024-11-18 21:50:45 -0500

    importing constant and unit subpackages. noted needed constants (at least so far)

--- WestDax_Project3.py
+++ WestDax_Project3.py
@@ -1,4 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import astropy as ap
+import astropy
 
+#importing constants
+from astropy import constants as const
+from astropy import units as unit
+#need planck constant (h), speed of light (c), mass of an electron (m_e), mass of a proton (m_p)
