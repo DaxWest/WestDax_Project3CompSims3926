809569735f9ca842a8229d15f5546f1ea69bd9be
DaxWest <awest49@uwo.ca>
2024-11-21 14:02:15 -0500

    first attempt at solution function

--- WestDax_Project3.py
+++ WestDax_Project3.py
@@ -1,10 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import astropy
+import scipy.integrate as scint
 
 #importing constants
 from astropy import constants as const
 from astropy import units as unit
+from astropy.units.quantity_helper.function_helpers import solve
+
 #need planck constant (h), speed of light (c), mass of an electron (m_e), mass of a proton (m_p)
 #mu_e is the number of nucleons (of mass m_p) per electron. this is not a constant
 
@@ -15,8 +18,11 @@ M_0 = 5.67e33 / ((mu_e)**2)
 rho_0 = (8 * np.pi / 3) * ((const.m_e * const.c / const.h)**3) * (const.m_p * mu_e)
 
 rho_min = 10**-33
+rad_wd_avg = 7e8 #7000km in cm since WD stars tend to be about the radius of the Earth and was are using cgs units
+zero_approx = 10**-100
+step = 1
 
-def system_eq_wd(r, ystate, R_0=1, M_0=1, rho_0=1):
+def system_eq_wd(r, ystate, R_0=R_0, M_0=M_0, rho_0=rho_0):
     rho, m = ystate
 
     RHO = rho / rho_0
@@ -31,8 +37,15 @@ def system_eq_wd(r, ystate, R_0=1, M_0=1, rho_0=1):
     return drho_dr, dm_dr
 
 #need to find the radius (range) of integration
-def density_range(r, ystate, rho_min=0):
+def density_range(r, ystate, rho_min=rho_min):
     if ystate[0] < rho_min:
         return 0
     return ystate[0]
+
+def solution_system_eq_wd(rho_max, rad_wd=rad_wd_avg, step_size=step, rad_min_approx=zero_approx):
+    range_density = [rho_max, 0]
+    range_radius = [rad_min_approx, rad_wd]
+    range_evaluation = np.linspace(rad_min_approx, rad_wd, step_size)
+
+    return scint.solve_ivp(system_eq_wd, range_radius, range_density, events=density_range)
 
