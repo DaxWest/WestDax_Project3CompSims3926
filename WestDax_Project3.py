a13bf3011562cbaec1a693ac86e948cfa1141b37
DaxWest <awest49@uwo.ca>
2024-11-21 13:11:00 -0500

    first attempt at making the function to solve the system of equations given as eq8 and eq9 in the instructions

--- WestDax_Project3.py
+++ WestDax_Project3.py
@@ -14,14 +14,17 @@ R_0 = 7.72e8 / mu_e
 M_0 = 5.67e33 / ((mu_e)**2)
 rho_0 = (8 * np.pi / 3) * ((const.m_e * const.c / const.h)**3) * (const.m_p * mu_e)
 
-x = (rho / rho_0)**(1/3)
-M = m / M_0
-R = r / R_0
 
-gamma_x = (x**2) / (3 * np.sqrt(1 + x**2))
+def system_eq_wd(r, ystate, R_0=1, M_0=1, rho_0=1):
+    rho, m = ystate
 
-#equation 8
-drho_dr = - (M * rho) / (gamma_x * r**2)
-
-#equation 9
-dm_dr = (R**2) * rho
+    RHO = rho / rho_0
+    x = (RHO) ** (1 / 3)
+    M = m / M_0
+    R = r / R_0
+    gamma_x = (x ** 2) / (3 * np.sqrt(1 + x ** 2))
+    # equation 8
+    drho_dr = - (M * RHO) / (gamma_x * R ** 2)
+    # equation 9
+    dm_dr = (R ** 2) * RHO
+    return drho_dr, dm_dr
