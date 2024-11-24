b5fe8cc72e46e58914c7da69f7e1dc9efca198ff
DaxWest <awest49@uwo.ca>
2024-11-23 20:19:21 -0500

    Decided to change the potted information to clearly show the upper limit of mass and radius while still showing (via dotted line) how the interior structure changes wrt the radius

--- WestDax_Project3.py
+++ WestDax_Project3.py
@@ -21,7 +21,7 @@ rho_0 = (8 * np.pi / 3) * ((m_e * c / h)**3) * (m_p * mu_e)
 
 #Question 1
 rho_min = 10**-1
-rad_wd_avg = 7e8 #7000km in cm since WD stars tend to be about the radius of the Earth and was are using cgs units
+rad_wd_avg = 7e6 #7000km in m since this is the upper limit of the radii of WD stars
 zero_approx = 10**-10
 step = 1
 
@@ -55,6 +55,10 @@ radius_sol = []
 density_sol = []
 mass_sol = []
 
+radius_sol_limit = []
+density_sol_limit = []
+mass_sol_limit = []
+
 for i in range(len(rho_c)):
     wd_sol = solution_system_eq_wd(rho_c[i], rad_wd=rad_wd_avg, step_size=step, rad_min_approx=zero_approx)
 
@@ -67,6 +71,14 @@ for i in range(len(rho_c)):
     density_sol.append(den_sol)
     mass_sol.append(m_sol)
 
+    rad_sol_limit = wd_sol.t_events[0][0] * R_0 / R_sun
+    den_sol_limit = wd_sol.y_events[0][0][0] * rho_0
+    m_sol_limit = wd_sol.y_events[0][0][1] * M_0 / M_sun
+
+    radius_sol_limit.append(rad_sol_limit)
+    density_sol_limit.append(den_sol_limit)
+    mass_sol_limit.append(m_sol_limit)
+
 #Question 2
 rho_legend = []
 for i in range(len(rho_c)):
@@ -75,18 +87,20 @@ for i in range(len(rho_c)):
 
 fig = plt.figure(figsize=(8,6))
 for i in range(len(density_sol)):
-    plt.plot(mass_sol[i], radius_sol[i])
+    plt.plot(mass_sol[i], radius_sol[i], ls=':')
 plt.title('Mass vs Radius for Various Maximum Internal Densities')
 plt.xlabel('Mass (solar masses)')
 plt.ylabel('Radius (solar radii)')
 plt.legend(rho_legend)
+for i in range(len(density_sol_limit)):
+    plt.scatter(mass_sol_limit[i], radius_sol_limit[i])
 plt.show()
 
 M_ch_estimation = []
 for i in range(len(mass_sol)):
     M_ch_estimation.append(np.max(mass_sol[i]))
 
-print(f'The estimation of the Chandrasekhar mass limit is: {np.max(M_ch_estimation)} Solar Masses')
+print(f'The estimation of the Chandrasekhar mass limit is: {Decimal(np.max(M_ch_estimation)):.4} Solar Masses')
 print(f'The true value of the Chandrasekhar mass limit is: {M_ch} Solar Masses')
 
 #Question 3
@@ -94,6 +108,10 @@ rho_c_3 = [rho_c[1], rho_c[4], rho_c[8]]
 radius_sol_3 = []
 density_sol_3 = []
 mass_sol_3 = []
+
+radius_sol_3_limit = []
+density_sol_3_limit = []
+mass_sol_3_limit = []
 
 for i in range(len(rho_c_3)):
     wd_sol_3 = solution_system_eq_wd(rho_c_3[i], rad_wd=rad_wd_avg, step_size=step, rad_min_approx=zero_approx, method='RK23')
@@ -106,15 +124,30 @@ for i in range(len(rho_c_3)):
     density_sol_3.append(den_sol_3)
     mass_sol_3.append(m_sol_3)
 
+    rad_sol_3_limit = wd_sol_3.t_events[0][0] * R_0 / R_sun
+    den_sol_3_limit = wd_sol_3.y_events[0][0][0] * rho_0
+    m_sol_3_limit = wd_sol_3.y_events[0][0][1] * M_0 / M_sun
+
+    radius_sol_3_limit.append(rad_sol_3_limit)
+    density_sol_3_limit.append(den_sol_3_limit)
+    mass_sol_3_limit.append(m_sol_3_limit)
+
 legend_method_comp = [f'$\\rho$ = {Decimal(rho_c[1]):.2e}', f'$\\rho$ = {Decimal(rho_c[4]):.2e}', f'$\\rho$ = {Decimal(rho_c[8]):.2e}']
 fig2, (ax1, ax2) = plt.subplots(1,2, figsize=(15,5))
 for i in range(len(density_sol_3)):
-    ax1.plot(mass_sol_3[i], radius_sol_3[i])
-ax2.plot(mass_sol[1], radius_sol[1])
-ax2.plot(mass_sol[4], radius_sol[4])
-ax2.plot(mass_sol[8], radius_sol[8])
+    ax1.plot(mass_sol_3[i], radius_sol_3[i], ls=':')
+ax1.legend(legend_method_comp)
+ax2.legend(legend_method_comp)
+for i in range(len(density_sol_3_limit)):
+    ax1.scatter(mass_sol_3_limit[i], radius_sol_3_limit[i])
+ax2.plot(mass_sol[1], radius_sol[1], ls=':')
+ax2.plot(mass_sol[4], radius_sol[4], ls=':')
+ax2.plot(mass_sol[8], radius_sol[8], ls=':')
+ax2.scatter(mass_sol_limit[1], radius_sol_limit[1])
+ax2.scatter(mass_sol_limit[4], radius_sol_limit[4])
+ax2.scatter(mass_sol_limit[8], radius_sol_limit[8])
 ax1.set_title('Method: RK23')
-ax2.set_title('Mathod: RK45')
+ax2.set_title('Method: RK45')
 ax1.legend(legend_method_comp)
 ax2.legend(legend_method_comp)
 ax1.set_xlabel('Mass (solar masses)')
@@ -136,9 +169,15 @@ with open('wd_mass_radius.csv', 'r') as wd_mass_data:
         wd_radius.append(float(cols[2]))
         wd_radius_unc.append(float(cols[3]))
 
-fig3 = plt.figure()
+fig3 = plt.figure(figsize=(8,6))
 for i in range(len(density_sol)):
-    plt.plot(mass_sol[i], radius_sol[i])
-plt.errorbar(wd_mass, wd_radius, xerr=wd_mass_unc, yerr=wd_radius_unc, color='k', ls='')
+    plt.plot(mass_sol[i], radius_sol[i], ls=':')
+plt.legend(rho_legend)
+plt.xlabel('Mass (solar masses)')
+plt.ylabel('Radius (solar radii)')
+plt.title('Observation vs Simulation Comparison')
+for i in range(len(density_sol_limit)):
+    plt.scatter(mass_sol_limit[i], radius_sol_limit[i])
 plt.scatter(wd_mass, wd_radius)
+plt.errorbar(wd_mass, wd_radius, xerr=wd_mass_unc, yerr=wd_radius_unc, color='k', ls='')
 plt.show()
